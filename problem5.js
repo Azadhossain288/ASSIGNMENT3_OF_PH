13880d9ac8ad5f00a685c36cae9c278325b856a8
Azadhossain288 <140039323+Azadhossain288@users.noreply.github.com>
2026-02-08 04:21:17 +0600

    Update problem5.js

--- problem5.js
+++ problem5.js
@@ -1,3 +1,45 @@
+
+Problem-05: Text Analyzer for an AI Company
+⚠️ Function Name Must be: analyzeText()
+
+Description
+Jihad works at an AI company. Often, users provide very large blocks of text as input. Processing such massive amounts of data takes the AI a lot of time. To optimize this, Jihad wants a function that analyzes and summarizes large texts by extracting key metrics.
+
+You need to write a function that:
+
+Takes a string as input.
+
+Finds the longest word in that text.
+
+Calculates the total number of characters in the text, excluding spaces.
+
+Input 📥
+A single input which must be a String (a full sentence or paragraph).
+
+Output 🚀
+An Object containing the following properties:
+
+longwords: A string representing the longest word in the sentence. (If there are multiple words of the same maximum length, return the first one encountered).
+
+token: A number representing the total character count of the sentence excluding spaces.
+
+Challenge 📢
+If the input is not a String (e.g., Number, Array, or Object), the function must return the string "Invalid".
+
+Hints 💡
+You can use the .split() method to break the sentence into individual words.
+
+You can use the .join() or .replace() methods to handle space removal for the character count.
+
+
+
+
+
+
+
+
+
+
 /*function signature/sample */
 function  analyzeText(str) {
           
