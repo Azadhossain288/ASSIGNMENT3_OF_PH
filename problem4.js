eac9cc710184f2fc26846f155a2c9b33ec5d042d
Azadhossain288 <140039323+Azadhossain288@users.noreply.github.com>
2026-02-08 04:18:32 +0600

    Update problem4.js

--- problem4.js
+++ problem4.js
@@ -1,3 +1,39 @@
+
+Problem-04: Upcoming Referendum (Gono Vote)
+⚠️ Function Name Must be: gonoVote()
+
+Description
+You have been assigned to analyze the results of an upcoming referendum (Gono Vote). The voting data is stored in an Array, where each voter's opinion is recorded as either "ha" (Yes) or "na" (No). Your task is to create a function that counts the votes and determines if the "ha" votes have the majority.
+
+Input 📥
+A single input which is expected to be an Array of strings. Example: ["ha", "na", "ha"]
+
+Output 🚀
+Returns true if the number of "ha" votes is greater than the number of "na" votes.
+
+Returns the string "equal" if the number of "ha" votes is exactly equal to the number of "na" votes.
+
+Returns false in all other cases (i.e., if "na" votes are the majority).
+
+Challenge 🚩
+If the input is not an Array (e.g., if it is a String, Number, or Object), the function must return the string "Invalid".
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*function signature/sample */
 function gonoVote(array) {
          
