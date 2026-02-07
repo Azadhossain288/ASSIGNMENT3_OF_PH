609584f6a5c33a24d114267d2d9a4299f0d373b9
Azadhossain288 <140039323+Azadhossain288@users.noreply.github.com>
2026-02-08 04:10:30 +0600

    Update problem1.js

--- problem1.js
+++ problem1.js
@@ -1,3 +1,31 @@
+
+Problem-01: New Price for Eid Sale
+⚠️ Function Name Must be: newPrice()
+
+Description
+For the upcoming Eid, you want to offer discounts on your shop's products. However, calculating the new price for every single item manually is a hassle. To solve this, you want to create a function that takes the original price and the discount percentage, and then calculates the new price.
+
+Input
+Current Price: A number representing the original price (currentPrice).
+
+Discount: A number representing the discount percentage (discount).
+
+Output
+The function should return the price after the discount is applied.
+
+The output can be a decimal number.
+
+The result must be formatted to 3 decimal places.
+
+Constraints/Challenge 📢
+If any of the inputs are not a number, return the string: "Invalid".
+
+If the discount amount is less than 0 or greater than 100, return the string: "Invalid".
+
+
+
+
+
 /*function signature/sample */
 function newPrice(currentPrice , discount ) {
           
